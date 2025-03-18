9efbccdc9e37760b0a8b0c285d5d9f6450557f30
Youngmin Jang <ym.jang@bubaum.com>
2025-03-18 16:59:21 +0900

    init

---
+++ src/main/java/com/example/jooqtest/JooqtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jooqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqtestApplication.class, args);
+	}
+
+}
